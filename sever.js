540028bbabc56279a2e4b968cd0f61ffb88646ce
shivam7374 <shivamsingla491@gmail.com>
2020-05-23 12:39:22 +0530

    Add

--- sever.js
+++ sever.js
@@ -1,10 +1,13 @@
 const express=require('express')
 const app=express()
+const PORT=process.env.PORT || 4444
 
 app.get('/',(req,res)=>{
     res.send('Hello World !!')
 })
 
-app.listen('4444',(req,res)=>{
-    console.log('http://localhost:4444')
+app.listen(PORT,(req,res)=>{
+    console.log(`http://localhost:${PORT}`) // this allows heroku to set the port 
+    // now we can write PORT=7666 node server.js then server works at server 7666
+    // so you can choose any server
 })
