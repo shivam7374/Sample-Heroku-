04959abf72b61ea09770c1a16b208c8c61cf4752
shivam7374 <shivamsingla491@gmail.com>
2020-05-23 15:38:19 +0530

    Sqlite db for heroku

---
+++ db.js
@@ -0,0 +1,27 @@
+const sequelize=require('sequelize')
+
+const db=new sequelize({
+    dialect:sqlite,
+    storage:__dirname+'/test.db'
+})
+
+const Tasks=db.define('task',{
+    id:{
+        type:sequelize.DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    name:{
+        type:sequelize.DataTypes.STRING,
+        allowNull:false
+    },
+    done:{
+        type:sequelize.DataTypes.BOOLEAN ,
+        defaultValue:falses
+
+    }
+})
+
+module.exports={
+    db,Tasks
+}
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express=require('express')
+const{ db, Tasks}=require('./db')
+
 const app=express()
 const PORT=process.env.PORT || 4444
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send('Hello World !!')
 })
+app.get('/tasks',async (req,res)=>{
+    res.send(await Tasks.findAll())
+})
 
-app.listen(PORT,(req,res)=>{
-    console.log(`http://localhost:${PORT}`) // this allows heroku to set the port 
-    // now we can write PORT=7666 node server.js then server works at server 7666
-    // so you can choose any server
+app.post('/tasks',async (req,res)=>{
+    res.send(await Tasks.create(req.body))
+})
+db.sync().then(()=>{
+
+    app.listen(PORT,(req,res)=>{
+        console.log(`http://localhost:${PORT}`) // this allows heroku to set the port 
+        // now we can write PORT=7666 node server.js then server works at server 7666
+        // so you can choose any server
+    })
 })
