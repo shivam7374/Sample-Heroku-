f8cc10e71378a24162c13d3bac32702ec7df3f4f
shivam7374 <shivamsingla491@gmail.com>
2020-05-24 10:13:13 +0530

    Sqlite db for heroku

--- db.js
+++ db.js
@@ -1,27 +1,30 @@
-const sequelize=require('sequelize')
+const Sequelize = require('sequelize')
 
-const db=new sequelize({
-    dialect:'sqlite',
-    storage: __dirname+'/test.db'
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: __dirname + '/test.db'
 })
 
-const Tasks=db.define('task',{
-    id:{
-        type:sequelize.DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true
-    },
-    name:{
-        type:sequelize.DataTypes.STRING,
-        allowNull:false
-    },
-    done:{
-        type:sequelize.DataTypes.BOOLEAN ,
-        defaultValue:false
-
-    }
+const Tasks = db.define('task', {
+  id: {
+    type: Sequelize.DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false
+  },
+  done: {
+    type: Sequelize.DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  priority: {
+    type: Sequelize.DataTypes.ENUM('high', 'low', 'normal'),
+    defaultValue: 'normal'
+  }
 })
 
-module.exports={
-    db,Tasks
+module.exports = {
+  db, Tasks
 }
